79ef197abd56c539f79542f5370a43bfd0005ef4
Kelian Dascher-Cousineau <kdascher@ucsc.edu>
2022-03-16 21:53:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Landslide_statistics
+
+Playing around with the observation of stick-slip events at the edge of a slow moving landslide
---
+++ ls_stat/utils.py
@@ -0,0 +1,32 @@
+from logging import setLogRecordFactory
+import warnings 
+from typing import Optional, Union
+
+import numpy as np
+
+# class Extensometer():
+#     def __init__(
+#         self,
+#         datenum: Union(np.ndarray,list),
+#         slip: Union(np.ndarray,list),      
+#         **kwargs,
+#     ):
+
+
+
+
+    # properties to create:
+    # slip
+    # time
+    # start time
+    # end time
+    # number of time steps
+    # events (time, slip)
+    # signal to noise threshoold
+    # threshold
+    
+    # methods:
+    # generate events(self,threshold,)
+    # ds/dt
+    # slice(t_start,t_end)
+    # 
